06c2d00adcbe3a2eb3324e60051671e8c8be2153
benche <ben.che9@gmail.com>
2018-01-29 12:55:42 -0500

    jquery + object practice using journal app - journal and entry objs created with tests

---
+++ journal-app/js/classes.js
@@ -0,0 +1,42 @@
+// constructors for Journal and Entry
+
+function Journal(name, entries) {
+    this.name = name;
+    this.entries = entries;
+    entries=[];
+    this.addEntry = function(title, content, author) {
+        this.entries.push(new Entry(title, content, author));
+    }
+    this.displayEntries = function() {
+        console.log(this.entries);
+    }
+}
+
+function Entry(title, content, author) {
+    this.title=title;
+    this.content=content;
+    this.author=author;
+}
+
+
+// debugging
+
+// tests for journal constructor
+let journalExample = new Journal('journal_name', []);
+console.log(journalExample.name + ' is the name of the journal');
+console.log(journalExample.entries + ' should be an empty array');
+
+// tests for entry constructor
+let entryExample = new Entry('entry_title_1', 'entry_1_content','ben');
+console.log(entryExample.title + ' is the title of the entry, and ' + entryExample.content + ' is the content, and ' + entryExample.author + ' is the author');
+
+// tests for journal methods
+journalExample.addEntry('entry_title_2', ' entry_content_2', 'entry_author');
+journalExample.addEntry('entry_title_3', ' entry_content_3', 'entry_author');
+console.log('test for addEntry method: ');
+console.log(journalExample.entries);
+
+console.log('test for displayEntry method: ');
+journalExample.displayEntries();
+
+
