b7b0a846fe72676e45edf00e6ff65dcab5c15d40
benche <ben.che9@gmail.com>
2018-01-31 17:16:32 -0500

    refactored code and comments and added short documentation to file

--- movie-web-scrapper/scraper.js
+++ movie-web-scrapper/scraper.js
@@ -1,7 +1,17 @@
-// spoiler machine
-//  takes in 2 arguments, a string and a num (in milliseconds)
-const cheerio = require('cheerio');
+// node packages
+
 const request = require('request');
+const $ = require('jQuery');
+const readlineSync = require('readline-sync');
+
+
+// simple cli program that uses the movie database api to get a list of recommended movies after
+//  user passes argument
+// global variables
+
+let userInput = "";
+let movieRecList = [];
+
 
 // ============= //
 // main fn call
@@ -13,27 +23,23 @@ movieRecommendation();
 // main fn documentation:
 // ==================== //
 
-// spoilerMachine takes in no arguments and returns nothing
-// fn (none) -> none
-//  side effect: console logs either an error message or a spoiler after a certain amount of time has passed
 function movieRecommendation () {
+
+    getMovieID(userInput);
     
-    let userInput = "Tron";
-    let movieRecList = [];
-    
-    parseDocument(userInput, movieRecList);
 
 }
 // ================ //
 // helper functions:
 // ================ //
 
-function parseDocument(userInput, movieRecList) {
-    // use variables to hold the movie name and spoiler time (converted to seconds from milliseconds)
+// function to get movie id for the api
 
-    // assign the destination url to a variable for request function
+function getMovieID(userInput) {
+    userInput = readlineSync.question('Make recommendations based off of what movie?')
+
     let movieUrl="https://api.themoviedb.org/3/search/movie?api_key=f07a88427ffc5183abd63adf3aa6c187&query=" + (userInput.split(" ").join("+"));
-    
+
     // this uses user input to get movie id
     request(movieUrl, function(error, response, body) {
 
@@ -42,19 +48,9 @@ function parseDocument(userInput, movieRecList) {
 
             // get movie id to use for search in query
             let movieID=((JSON.parse(body)).results[1]).id;
-            console.log(movieID);
             let movieRecUrl = 'https://api.themoviedb.org/3/movie/'+movieID+'/recommendations?page=1&language=en-US&api_key=f07a88427ffc5183abd63adf3aa6c187';
-
-            // this uses movie id attained above to get other recommendations
-            request(movieRecUrl, function(error, response, body) {
-                
-                for (let i = 0; i < JSON.parse(body).results.length; i++){
-                    movieRecList.push(JSON.parse(body).results[i].title);
-                    console.log(JSON.parse(body).results[i].title);
-                }
-                
-            })
-        } 
+            getRecommendations(movieID);
+        }  
 
         // do these if an error occurs or the movie was not found in the database
         else if (error) {
@@ -63,5 +59,29 @@ function parseDocument(userInput, movieRecList) {
         else {
             console.log("your movie was not found in our database");
         }
-    }
-    )}
+    })
+}
+
+// fn to get movie recomendations based on id
+// mutates global var movieRecList
+
+function getRecommendations(movieID) {
+
+    // variable to hold movie reccomendation api endpoint
+    let movieRecUrl = 'https://api.themoviedb.org/3/movie/'+movieID+'/recommendations?page=1&language=en-US&api_key=f07a88427ffc5183abd63adf3aa6c187';
+    
+    // get & parse through data
+    request(movieRecUrl, function(error, response, body) {
+        for (let i = 0; i < JSON.parse(body).results.length; i++){
+            movieRecList.push(JSON.parse(body).results[i].title);
+        }
+        // test to see if reccomended movies show up
+        console.log('Recommended movies based on ' +userInput)
+        
+        movieRecList.forEach(function(element){
+            console.log(element);
+        })
+        
+    })
+}
+
