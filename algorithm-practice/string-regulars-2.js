72acdd08967100faf6c63a9c15e7c13626669aa0
benche <ben.che9@gmail.com>
2018-02-05 20:09:11 -0500

    additional regular expression practice

---
+++ algorithm-practice/string-regulars-2.js
@@ -0,0 +1,14 @@
+//Write a function takes a str parameter and returns true if the characters “a” and “b” are 
+//separated by exactly 3 places anywhere in the string at least once (ie. "lane borrowed" 
+//would result in true because there is exactly three characters between a and b).
+//Otherwise return the string false. Input:"after badly" Output:"false" Input:"Laura sobs" Output:"true"
+
+function kindaNeighbours(str) {
+    return !/(a...b)/.test(str);
+}
+
+// returns false
+console.log(kindaNeighbours('laura sobs'));
+
+// returns true
+console.log(kindaNeighbours("after badly"));
