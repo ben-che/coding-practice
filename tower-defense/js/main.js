53f0963e862b90cb349d9d4958660f91e46362d0
benche <ben.che9@gmail.com>
2018-03-18 22:22:12 -0400

    hp and thit detection implemented, as well as skins...pathing is still a mess

--- tower-defense/js/main.js
+++ tower-defense/js/main.js
@@ -53,7 +53,7 @@ const MOVE_E = 2;
 const MOVE_W = -2;
 const MOVE_LAST = -99;
 
-
+// how many creeps are on the board each wave
 let creepCounter = 10;
 
 // Game Logic
@@ -99,30 +99,60 @@ let runGame = (event) => {
     // movement logic
     let creepDirection = new Array(); // array that holds the direction for each minion to move in
     let creepSpawnCount = 1; // how many creeps have spawned thusfar
-    let creepsOnBoard = new Array();
+    let creepsOnBoard = new Array();    // add every time a new creep is spawned
 	let creepHp = new Array();  // array that holds all hp of creeps
 	let creepsKilled = new Array(); // true/false to indicate which creep is dead/alive
 	let creepsDead = 0; 
     let waveOver = false;   // indicates when each wave is over - either all creeps are dead or player is
+    let livesLost = 0;      // how many lives have been lost thusfar
 
     // reset creeps
     let creeps = document.getElementsByClassName("gen-creep");
-    console.log(creeps)
+    // console.log(creeps)
+    // assign the following values to each crepe that spawns
 	for(let i = 0; i < creeps.length; i++) {
 		moveX[i] = 0;
 		moveY[i] = 0;
 		creepDirection[i] = MOVE_S;
 		creepsOnBoard[i] = 0;
 		creeps[i].style.display = "none";	
-		creepHp[i] = Math.pow(2, levelWave)*100;
-		creepsKilled[i] = true;
+        // creepHp[i] = Math.pow(2, levelWave)*100;
+        creepHp[i] = 1;  // 1 hp for testing
+        creepsKilled[i] = true;
     }
     
     // interval function that moves creeps every 30ms
     timeTick = setInterval(() => {
-        for (let i = 0; i < creepCounter; i++) {
+        for (let i = 0; i < creepSpawnCount; i++) {
             creepDirection[i] = creepMovement(moveX[i], moveY[i], creepDirection[i]);
-            console.log(creepDirection[i]);
+            // console.log(creepDirection[i]);  
+            
+            // check if creep fell off map
+            if (creepDirection[i] === MOVE_LAST) {
+
+                // creep leaves map
+                if (creeps[i].style.display === 'none') {
+                    // lose a life
+                    playerLives -= 1;
+                    livesLost++;
+                }
+                // creep reaches end of map
+                creeps[i].style.display = 'none';
+
+                // player runs out of lives
+                if (playerLives <= 0) {
+                    waveOver = true;
+                    start = false;
+                    break;
+                }
+                // once all the creeps have been killed, we can progress onto the next round!
+                if (creepsDead === (creeps.length - livesLost)) {
+                    waveOver = true;
+                }
+
+                continue;
+
+            }
 
             // see which way to move, and update the data in the array
             switch(creepDirection[i]) {
@@ -134,6 +164,7 @@ let runGame = (event) => {
                     break;
                 case MOVE_E:
                     moveX[i] += 1;
+                    // console.log(moveX[i]);
                     break;
                 case MOVE_W:
                     moveX[i] -= 1;
@@ -144,8 +175,77 @@ let runGame = (event) => {
             creeps[i].style.display = "block";	
             creeps[i].style.top = moveY[i] + "px";	
             creeps[i].style.left = moveX[i] + "px";
+
+            // check to see if there are any enemies in range of turrets
+            let damage = checkTurretRange(creeps[i], moveX[i], moveY[i]);
+            // subtract damage from remaining creep hp
+            creepHp[i] -= damage;
+            console.log('creep hp:', creepHp[i]);
+            // check to see if creep is dead
+            if (creepHp[i] <=0 ) {
+                creeps[i].style.display='none'
+                // see if creep is still alive 
+                if (creepsKilled[i]) {
+                    // creep is no longer alive
+                    creepsKilled[i] = false;
+                    creepsKilled++;
+
+                    // add cash - base is 10, with additional increments per wave level
+                    playerCash += Math.floor((10 + (waveLevel*2.5)));
+
+                    // if all creeps that are spawned are off the board (either killed
+                    //  or reached end of board), the wave is over
+                    if (creepsKilled === (creeps.length - livesLost)) {
+                        waveOver = true;
+                    }
+                    creeps[i].style.display = 'none';
+                }
+                // spawn creeps every 1.5 seconds until all creeps created earlier are on the board
+                if ((creepsOnBoard[i] === 150*creepSpawnCount) && creepSpawnCount < creeps.length) {
+                    creepCounter++;
+                }
+                creepsOnBoard[i]++;
+            }
+            // update the cash display every tick to ensure that the game displays updated playerCash
+            let cash = document.getElementById("cash");
+            cash.innerHTML = "$" + playerCash;
+
+            // update the wave number
+            var wave = document.getElementById("wave");
+            wave.innerHTML = levelWave;
+
+            // update the amount of lives the player has
+            var lives = document.getElementById("lives");
+            lives.innerHTML = playerLives;
+
+            // check to see if the wave is over
+            if (waveOver) {
+                //  case where player loses
+                if (playerLives === 0) {
+                    let lives = document.getElementById('lives');
+                    lives.innerHTML = 'dead'
+                    // stop the game because palyer loses
+                    clearInterval(timeTick);
+                }
+                // reset variables for next wave
+                creepSpawnCount = 1;
+                creepsKilled = 0;
+                waveOver = false;
+                currentWave++;
+                // reset creep-specific variables
+                for(let i = 0; i < creeps.length; i++) {
+                    moveX[i] = 0;
+                    moveY[i] = 0;
+                    creepDirection[i] = MOVE_S;
+                    creepsOnBoard[i]=0;
+                    creeps[i].style.display = 'none';
+                    // creepHp[i] = Math.pow(2, levelWave)*100;
+                    creepHp[i] = 1;
+                    creepsKilled[i] = true;
+                }
+            }
         }
-    }, 30)
+    }, 1)
     
     
 }
@@ -155,49 +255,69 @@ let runGame = (event) => {
 // takes in an instance of the creep's x coordinates, y coordinates and the current creep direction
 let creepMovement = (x, y, dir) => {
     // convert x and y and scale it to each tile's 
-    let curX = Math.floor((x + tileWidth / 2) / tileWidth);
-    let curY = Math.floor((y + tileHeight / 2) / tileHeight);
+    x = (x + tileWidth / 2) / tileWidth;
+    y = (y + tileHeight / 2) / tileHeight;
 
-    let newX = Math.floor(curX);
-    let newY = Math.floor(curY);
+    let newX = Math.floor(x);
+    let newY = Math.floor(y);
+    // console.log(newX);
+    // console.log(newY)
 
+    // check what direction we're going and check to see if a valid tile in that direction exists
+    switch(dir) {
+        case MOVE_N:
+            newY -= 1;
+            break;
+        case MOVE_S:
+            newY += 1;
+            break;
+        case MOVE_E:
+            newX += 1;
+            break;
+        case MOVE_W:
+            newX -= 1;
+            break;
+    }
+    
     // next steps are to check which level we're on and use that level's map to check which
     //  way the creeps will move - as new levels are added, this conditional block will need
     //  to be updated as well - as it currently stands, there is only one level
 
     switch(levelNum) {
         case 0:
-        console.log('level zero hit');
+        // console.log('level zero hit');
+        // if the new location does exist, then keep going, all is well
+        if (levelZero(Math.floor(newX), Math.floor(newY))) {
+            // console.log('moving in the same direction')
+            return dir;
+        }
         // if the new position does not exist, it means the creep will fall off the map - we
         //  have to redirect it
         // if a tile east of the creep's current location exists, and if the new movement isn't
         //  the opposite of east (otherwise the creep would be moving backwards), then allow the
         //  creep to move east
-        if ( (levelZero(newX + 1), newY) && dir != MOVE_W) {
-            console.log('moving e')
+        if ( (levelZero(Math.floor(x) + 1), Math.floor(y)) && dir != -MOVE_E) {
+            // console.log( (levelZero(Math.floor(curX) + 1), Math.floor(curY)) && dir != -MOVE_E);
+            // console.log('moving e')
             return MOVE_E;
         }
-        if ( (levelZero(newX - 1, newY)) && dir != MOVE_E) {
+        if ( (levelZero(Math.floor(x) - 1, Math.floor(y))) && dir != -MOVE_W) {
             console.log('moving w')
             return MOVE_W;
         }
-        if ( levelZero(newX, newY - 1) && dir != MOVE_N ) {
+        if ( levelZero(Math.floor(x), Math.floor(y) - 1) && dir != MOVE_S ) {
             console.log('moving s')
             return MOVE_S;
         }
-        if ( levelZero(newX, newY + 1) && dir != MOVE_S ) {
+        if ( levelZero(Math.floor(x), Math.floor(y) + 1) && dir != MOVE_N ) {
             console.log('moving n')
             return MOVE_N;
         }
-        // if the new location does exist, then keep going, all is well
-        if (levelZero(newX, newY)) {
-            console.log('moving in the same direction')
-            return dir;
-        }
+        // if the board doesn't exist anywhere else, it means the creep has reached the end of their path
+        return MOVE_LAST;
     }
 
-    // if the board doesn't exist anywhere else, it means the creep has reached the end of their path
-    return MOVE_LAST;
+    
 }
 
 // drawMap() takes in no arguments and returns no values, but will create a grid system for the rest of the 
@@ -224,7 +344,8 @@ const drawMap = () => {
             //  enemies will travel - refer to levelZero function for more information
             //  we will pass levelZero x and y coordinates accordingly
             if ((levelNum === 0) && (levelZero(j, i))) {
-                mapTile.style.backgroundColor = "#1E90FF";
+                // set dirt path background for creeps to navigate on
+                mapTile.style.background = "url('img/Dirt.jpg')";
             }
             // if the tile isn't part of the enemy path, it means that it will be
             //  a tile that we can place a turret on - we will need to add event listeners
@@ -279,8 +400,9 @@ const drawMap = () => {
 	startButton.setAttribute("id","start-button");
 	startButton.setAttribute("class","gen-button-ui start-button");
 	startButton.innerHTML = "<p> start game </p>";
-	// document.getElementById('start-button').addEventListener('click', startGame);
     document.body.appendChild(startButton);
+    // clicking start button starts the game
+    document.getElementById('start-button').addEventListener('click', startGame);
     // console log:
     // console.log('start button created');
  
@@ -303,6 +425,12 @@ const drawMap = () => {
     // console.log('status bar created');
 }
 
+// event listener placed on the start button for starting the game
+let startGame = () => {
+    // event.preventDefault();
+    runGame();
+
+}
 // =============== //
 // Map Generation  //
 // =============== //
@@ -314,16 +442,20 @@ const drawMap = () => {
 // visually, if true is returned, the tile will be turned into a path tile, if not
 //  then the tile will be a grass tile for turret placement
 const levelZero = (x,y) => {   
+    // console.log(x, y);
+
     // defining the coordinates for enemy pathing
-    if(	(x === 0 && (y >= 0 && y <= 1)) ||
-        (x >=1 && x <= 75) && (y === 1) ||
-        (x === 75 && (y >= 1 && y <= 5)) ||
-        (x <= 75 && x >= 1) && (y===5) ||
-        (x===1 && y>=5)
-		) {
-            return true;
-        }
-        return false;
+    if(	( ((x === 0) && (y >= 0 && y <= 4))) ||
+        ( ((x >= 0 && x <= 74) && (y === 4))) || 
+        ( ((x === 75) && ( y >= 4 && y <= 6))) ||
+        ( ((x <= 75 && x >= 0) && ( y === 6 )))
+    )
+	{  
+        // console.log('returning true')
+		return true;
+    }
+    // console.log('returning false')
+	return false;
 }
 
 
@@ -393,7 +525,6 @@ const turretBuy = (id) => {
     spawnTurret.addEventListener('dragstart', (event) => {turretDrag(spawnTurret, event)});
     document.body.appendChild(spawnTurret);
 
-    
 
 }
 
@@ -467,6 +598,37 @@ let dropTurret = (event) => {
     // once the player places the turret, it stays in place		
     turretContext.setAttribute("draggable","false");
     turretContext.addEventListener('dragstart', (event) => cancelEvent(event));
+}
+
+let checkTurretRange = (creep, x, y) => {
+    let damage = 0;
+    
+    for (let i = 0; i < turretCounter; i++) {
+        console.log('num of turrets logged in var:', turretCounter);
+        console.log('num of turrets actually in array: ', turretPosition.length)
+        // turretPosition array elements are composed of :
+        //  damage, range, x-location, y-location, and unqiue id in that order
+        let turretX = turretPosition[i][2];
+        let turretY = turretPosition[i][3];
+
+        if (radiusCalc(x, turretX, y, turretY) <= turretPosition[i][1]) {
+            // creep takes damage - flash red as visual indicator
+            creep.style.backgroundColor='red';
+            damage += turretPosition[i][0]; 
+        }
+    }
+    // if creep is not taking damage, keep background transparent
+    if (damage === 0) {
+        creep.style.backgroundColor='transparent';
+    }
+    // damage is being logged correctly
+    // console.log(damage)
+    return damage;
+}
+
+// calculate
+let radiusCalc = (x1,x2,y1,y2) => {
+    return Math.sqrt(Math.pow(x1-x2,2) + Math.pow(y1-y2,2));
 }
 
 // turret drag logic end
