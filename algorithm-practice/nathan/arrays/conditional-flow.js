a2104a72b86af74cff65606a51b9ef09dd67805a
benche <ben.che9@gmail.com>
2018-08-09 15:19:23 -0400

    ternary and switch added

--- algorithm-practice/nathan/arrays/conditional-flow.js
+++ algorithm-practice/nathan/arrays/conditional-flow.js
@@ -28,4 +28,41 @@ let ifElseExample = () => {
     }
 }
 
-// Write the number / letter grades example from above:
+// Writing the number / letter grades example from above:
+
+
+// Switch statements:
+//  Used when you know specific cases that the arguments will fall into
+//  The general idea is the same as an if/else though - if a case is true,
+//  then go within that case's block, and if not, then move on to the next case
+//  An example of switch statements can be found in restaurant orders:
+//  A customer orders Sandwich C. The chef checks to see which case the customer
+//  orders, and then starts to prepare the sandwich
+//  Syntax goes as follows:
+let switchExample = (sandwich) => {
+    switch(argument) {
+        case (argument === "One") :
+            console.log(1)
+            break;
+        case (argument === "Two"):
+            console.log(2)
+            break;
+        default:
+            console.log("none");
+    }
+}
+
+// Writing the function:
+
+// Ternary Operator:
+//  Used as a quick way to write a true/false - it's used to React to render
+//  one thing vs rendering another (think of a modal)
+//  Generally, we want to keep the true/false behaviours very short for readability;
+//  we shouldn't be defining entire functions or have multiple outputs in a ternary
+//  for readability reasons
+//  Syantax:
+
+// If a is true, then doTrue(), if a is false, then doFalse()
+let ternaryExample = () => {
+    a ? doTrue() : doFalse()
+}
