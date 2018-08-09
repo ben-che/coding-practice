ffc9960c0ea2cc3845fe1043e01ef2b03951a234
benche <ben.che9@gmail.com>
2018-08-09 15:22:28 -0400

    true false added

---
+++ algorithm-practice/nathan/arrays/truefalse.js
@@ -0,0 +1,20 @@
+// True / False operators:
+// What do the following statements evaluate to?
+
+1 > 2   //
+
+1 < 2   //
+
+1 < 2 && 4 === 4 //
+
+1 === 1 && 2 < 1 //
+
+1 === 2 || 2 === 2 && 1 === 2 //
+
+1 //
+
+2 //
+
+0 //
+
+-10 //
