cce41222ff04defe27e57a084b8bb0ee079165db
benche <ben.che9@gmail.com>
2019-01-27 12:06:44 -0500

    unique min sum

---
+++ algorithm-practice/unique-min-sum.js
@@ -0,0 +1,3 @@
+// Given an array, you must increment any duplicate elements until all its elements are unique. In addition,
+// the sum of its elements must be the minimum possible within the rules. For example, if arr = [3, 2, 1, 2, 7],
+// then arr = [3, 2, 1, 4, 7] and its elements sum to a minimal value of 3 + 2 + 1 + 4 + 7 = 17.
