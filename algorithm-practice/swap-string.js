fe4ceeb5a4d1f33f02af72ab7cea7504c721747b
benche <ben.che9@gmail.com>
2019-01-26 23:25:45 -0500

    swap string solution

--- algorithm-practice/swap-string.js
+++ algorithm-practice/swap-string.js
@@ -11,10 +11,63 @@
 // based on whether they are twins. There should be one string added to the array per test in the order the
 // pairs are given.
 
-// Input: 
+// Input:
 // a = [['c','d','a','b'], ['d','c','b','a'], ['a','b','c','d']]
 // b = [['a','b','c','d'], ['a','b','c','d'], ['a','b','c','d','c','d']]
 
 // Output:
 // [true, false, false]
 
+// Proposed solution:
+
+// push all even elements inside a[i] into an array, push all even elements inside b[i] into another array
+// compare the two arrays - if they don't contain the same elements, return false
+// do the same for all odd elements next
+
+function twins(a, b) {
+	let ans = [];
+	for (let i = 0; i < a.length; i++) {
+		if (isEqual(a[i], b[i])) {
+			ans.push(true);
+		} else {
+			ans.push(false);
+		}
+	}
+	console.log(ans);
+}
+// helper function to check equality of two arrays
+function isEqual(a, b) {
+	let oddA = [];
+	let oddB = [];
+	let evenA = [];
+	let evenB = [];
+	if (a.length !== b.length) {
+		return false;
+	}
+
+	for (let i = 0; i < a.length; i++) {
+		if (i % 2 === 0) {
+			evenA.push(a[i]);
+			evenB.push(b[i]);
+		} else {
+			oddA.push(a[i]);
+			oddB.push(b[i]);
+		}
+    }
+    
+    for (let i = 0; i < a.length; i++) {
+        if (a.indexOf(b[i]) < 0) {
+            return false
+        }
+        if (b.indexOf(a[i]) < 0) {
+            return false
+        }
+    }
+
+	return true;
+}
+
+twins(
+	[['c', 'd', 'a', 'b'], ['d', 'c', 'b', 'a'], ['a', 'b', 'c', 'd']],
+	[['a', 'b', 'c', 'd'], ['a', 'b', 'c', 'd'], ['a', 'b', 'c', 'd', 'c', 'd']]
+);
