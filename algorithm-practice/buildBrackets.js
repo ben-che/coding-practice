f08bb3091549175089624b11bc9cfcc11409f833
benche <ben.che9@gmail.com>
2019-02-10 15:06:59 -0500

    build brack solution

--- algorithm-practice/buildBrackets.js
+++ algorithm-practice/buildBrackets.js
@@ -3,3 +3,27 @@
 // 1: ()
 // 2: ()(), (())
 // 3: ((())), (())(), ()(()), ()()(), (()())
+
+function buildBrackets(n) {
+	bracketHelper('', n, n);
+}
+
+function bracketHelper(str, openNumber, closeNumber) {
+	let newStr = str;
+	if (openNumber <= 0 && closeNumber <= 0) {
+		console.log(newStr);
+	}
+	if (openNumber >= 1) {
+		bracketHelper(newStr + '(', openNumber - 1, closeNumber);
+	}
+	if (closeNumber >= 1 && openNumber < closeNumber) {
+		bracketHelper(newStr + ')', openNumber, closeNumber - 1);
+	}
+}
+
+console.log('n = 1');
+buildBrackets(1);
+console.log('n = 2');
+buildBrackets(2);
+console.log('n = 3');
+buildBrackets(3);
